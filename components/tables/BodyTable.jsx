155d10ccd5bd20653229ae760b942489a77d475e
Mari <wourtex99@gmail.com>
2022-11-05 18:10:35 +0300

    feat: add dots

--- components/tables/BodyTable.jsx
+++ components/tables/BodyTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TableBody, TableRow, TableCell } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export const BodyTable = ({data}) => {
   return(
@@ -9,7 +10,17 @@ export const BodyTable = ({data}) => {
           <TableRow key={row} 
           sx={{ borderBottom: '1.1px solid #cc2222', '&:last-child td, &:last-child th': {borderBottom: '1.1px solid #fff'}}}>
             {
-              Object.values(row).map((td) => (<TableCell>{td}</TableCell>))
+              Object.values(row).map((td) => (
+                <TableCell>{td}</TableCell>
+                ))
+            }
+            {
+              row['spgz'] ? <MoreVertIcon  sx={{
+                mt: 2,
+                ml: -4,
+                cursor: 'pointer',
+              }}/> 
+              : null
             }
           </TableRow>
         ))
